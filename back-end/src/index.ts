fc2fe077a5fbddf8c06488ad5dc6789d1b7a9f84
Rafael Noll <rafanoll2004@gmail.com>
2025-01-19 13:07:06 -0300

    Chore: Adding cors config

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express'
 import 'express-async-errors';
 import 'dotenv/config'
 import './startup';
+import cors from 'cors';
 
 import routes from './routes';
 import { handleErrors } from './middlewares/handleErrors';
@@ -12,6 +13,7 @@ const app: Express = express()
 const port = process.env.SERVER_PORT || 3000;
 
 // Middlewares
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', handleLoggedUser, express.static(path.join('uploads')));
